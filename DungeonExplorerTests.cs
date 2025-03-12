33a7566ea07a5d286083229e15b0cdbbf8026cc6
bellahorns <29117112@students.lincoln.ac.uk>
2025-03-12 22:07:04 +0000

    Created the Test Class

---
+++ DungeonExplorerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Media;
+
+namespace DungeonExplorer
+{
+    internal class DungeonExplorerTests
+    {
+        public void TestPuckUpPotions()
+        {
+        
+            var player = new Player();
+            var initialPotionCount = player.potions;
+
+            Player.pickUpItem("Potion");
+
+            bool result = (initialPotionCount == initialPotionCount + 1);
+            Debug.Assert(result, "Potion count increases by too much.");
+        }
+    }
+}
